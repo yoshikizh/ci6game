e5e5f1914e2476cad4fe7204553070d3c7a87ea9
yoshikizh <177365340@qq.com>
2020-03-27 15:56:04 +0800

    remove init_dom func & init template html

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,15 @@
-require("./dom_init");
-require("./components_init");
+import React from "react";
+import ReactDOM from "react-dom";
+import {AppHeaderInit, AppBodyInit, AppFooterInit} from "./app_init";
+
+// render 组件初始化入口函数
+function renderComponentsEntry() {
+  let dom_header = document.getElementsByTagName("header")[0];
+  let dom_container = document.getElementById("container");
+  let dom_footer = document.getElementsByTagName("footer")[0];
+  ReactDOM.render(React.createElement(AppHeaderInit), dom_header);
+  ReactDOM.render(React.createElement(AppBodyInit), dom_container);
+  ReactDOM.render(React.createElement(AppFooterInit), dom_footer);
+};
+
+renderComponentsEntry();
