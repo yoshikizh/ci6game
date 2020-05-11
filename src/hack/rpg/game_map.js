d6ae141f39c5c3abc68bc3423d8e657331919d97
yoshikizh <177365340@qq.com>
2020-05-11 20:18:52 +0800

    map mode tilemap A1 finished.

--- src/hack/rpg/game_map.js
+++ src/hack/rpg/game_map.js
@@ -1,3 +1,4 @@
+
 Game_Map.prototype.findEvent = function(x,y){
   return this.events().filter((e)=>{ return e.x === x && e.y === y})[0];
 };
@@ -8,4 +9,8 @@ Game_Map.prototype.existEvent = function(x,y){
 
 Game_Map.prototype.existEventExcept = function(except_event,x,y){
   return this.events().filter((e)=>{ return e._eventId !== except_event._eventId && e.x === x && e.y === y}).length > 0;
+};
+
+Game_Map.prototype.gridCount = function(x,y){
+  return this.width() * this.height();
 };
