0de4ba8dbc169421ec47dc3f71b826684c92e46d
yoshikizh <177365340@qq.com>
2020-05-16 22:10:40 +0800

    move some funs to super class.

--- src/hack/rpg/module/tile_handle.js
+++ src/hack/rpg/module/tile_handle.js
@@ -1,6 +1,3 @@
-// import TileHandleA1 from "./tile_handle_a1";
-// import TileHandleA2 from "./tile_handle_a2";
-
 class TileHandle {
 
   constructor(x,y){
@@ -30,6 +27,39 @@ class TileHandle {
     const pos_index = x + y * map_width;
     return $dataMap.data[pos_index + layer * $gameMap.gridCount()];
   }
+
+  // 根据index查询tile_id是否为范围内(48个组成之一)
+  isCurrentTileByIndex(index,tile_id,layer){
+    if (index < 0) return false;
+    const target_tile_id = $dataMap.data[index + layer * $gameMap.gridCount()];
+    return target_tile_id >= tile_id && target_tile_id < tile_id + 48;
+  }
+
+  // 根据pos查询tile_id是否为范围内(48个组成之一)
+  isCurrentTile(x,y,tile_id,layer){
+    const target_index = x + y * $gameMap.width();
+    return this.isCurrentTileByIndex(target_index,tile_id,layer);
+  };
+
+  // 通过 index 获取 tile_id 的 index
+  getTileIndexByTileId(tile_id){
+    return parseInt((tile_id - this.start_tile_id) / 48);
+  };
+
+  // 通过 pos 获取 tile_index
+  getTileIndexByTilePos(x,y,layer){
+    const tile_id = this.getTileIdByPos(x,y,layer);
+    const tile_index = this.getTileIndexByTileId(tile_id);
+    return tile_index;
+  };
+
+  clearTileLayerByPos(x,y,layer){
+    const target_index = x + y * $gameMap.width();
+    const clear_target_id = $dataMap.data[target_index + layer * $gameMap.gridCount()];
+    if (clear_target_id !== 0){
+      $dataMap.data[target_index + layer * $gameMap.gridCount()] = 0;
+    }
+  };
 
   // 通过x,y绘制tile
   drawTileByPos(x,y){
@@ -68,6 +98,230 @@ class TileHandle {
       }
     }
   }
+
+  // [自动图块使用] 扫描检测周边格子是满足条件, conditions 为一组周边8个格子的情况 -> 0: 不包含 1: 包含  null: 不检测跳过
+  // 超过地图边界这返回 -> 包含
+  conditionScanRoundAutoTiles(conditions, x, y, tile_id, layer){
+
+    const pos_left  = [x-1, y];
+    const pos_up    = [x, y-1];
+    const pos_right = [x+1, y];
+    const pos_down  = [x, y+1];
+    const pos_left_top   = [x-1, y-1];
+    const pos_right_top  = [x+1, y-1];
+    const pos_left_down  = [x-1, y+1];
+    const pos_right_down = [x+1, y+1];
+
+    let result = true;
+    for(let i = 0; i < conditions.length; i++){
+      if (conditions[i] === null){
+        continue;
+      }
+      let dir = i + 1;
+      let exist = conditions[i] === 1;
+
+      if (dir === 1){
+        if (exist !== (this.checkTileIsPadding(pos_left_down[0],pos_left_down[1]) || this.isCurrentTile(pos_left_down[0],pos_left_down[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 2){
+        if (exist !== (this.checkTileIsPadding(pos_down[0],pos_down[1]) || this.isCurrentTile(pos_down[0],pos_down[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 3){
+        if (exist !== (this.checkTileIsPadding(pos_right_down[0],pos_right_down[1]) || this.isCurrentTile(pos_right_down[0],pos_right_down[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 4){
+        if (exist !== (this.checkTileIsPadding(pos_left[0],pos_left[1]) || this.isCurrentTile(pos_left[0],pos_left[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 6){
+        if (exist !== (this.checkTileIsPadding(pos_right[0],pos_right[1]) || this.isCurrentTile(pos_right[0],pos_right[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 7){
+        if (exist !== (this.checkTileIsPadding(pos_left_top[0],pos_left_top[1]) || this.isCurrentTile(pos_left_top[0],pos_left_top[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 8){
+        if (exist !== (this.checkTileIsPadding(pos_up[0],pos_up[1]) || this.isCurrentTile(pos_up[0],pos_up[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      if (dir === 9){
+        if (exist !== (this.checkTileIsPadding(pos_right_top[0],pos_right_top[1]) || this.isCurrentTile(pos_right_top[0],pos_right_top[1],tile_id,layer))){
+          result = false;
+          break;
+        }
+      }
+      
+    }
+    return result;
+  }
+
+  // [自动图块使用] 根据周边tile_id状况来获取中心tile的内部tile_id
+  getCenterAutoTileIdByAround(x,y,tile_id,layer){
+
+    let center_inner_target_id;
+    if (this.conditionScanRoundAutoTiles([1,1,1,1,null,1,1,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 0;
+    } 
+    else if (this.conditionScanRoundAutoTiles([1,1,1,1,null,1,0,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 1;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,1,1,null,1,1,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 2;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,1,1,null,1,0,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 3;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,0,1,null,1,1,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 4;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,0,1,null,1,0,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 5;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,0,1,null,1,1,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 6;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,0,1,null,1,0,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 7;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,1,1,null,1,1,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 8;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,1,1,null,1,0,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 9;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,1,1,null,1,1,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 10;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,1,1,null,1,0,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 11;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,0,1,null,1,1,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 12;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,0,1,null,1,0,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 13;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,0,1,null,1,1,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 14;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,0,1,null,1,0,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 15;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,1,0,null,1,null,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 16;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,1,0,null,1,null,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 17;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,0,0,null,1,null,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 18;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,0,0,null,1,null,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 19;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,1,1,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 20;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,0,1,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 21;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,1,1,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 22;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,0,1,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 23;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,null,1,null,0,1,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 24;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,null,1,null,0,1,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 25;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,null,1,null,0,0,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 26;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,null,1,null,0,0,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 27;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,1,1,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 28;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,1,0,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 29;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,1,1,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 30;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,1,0,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 31;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,null,0,null,0,null,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 32;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 33;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,1,0,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 34;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,null,0,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 35;
+    }
+    else if (this.conditionScanRoundAutoTiles([1,1,null,1,null,0,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 36;
+    }
+    else if (this.conditionScanRoundAutoTiles([0,1,null,1,null,0,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 37;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,0,1,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 38;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,0,0,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 39;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,0,null,1,null,1,1],x,y,tile_id,layer)){
+      center_inner_target_id = 40;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,0,null,1,null,1,0],x,y,tile_id,layer)){
+      center_inner_target_id = 41;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,1,null,0,null,0,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 42;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,0,null,1,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 43;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,0,null,0,null,1,null],x,y,tile_id,layer)){
+      center_inner_target_id = 44;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,1,null,0,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 45;
+    }
+    else if (this.conditionScanRoundAutoTiles([null,0,null,0,null,0,null,0,null],x,y,tile_id,layer)){
+      center_inner_target_id = 46;
+    }
+    return center_inner_target_id;
+  }
+
+
 }
 
 export default TileHandle;
--- src/hack/rpg/module/tile_handle_a1.js
+++ src/hack/rpg/module/tile_handle_a1.js
@@ -36,30 +36,6 @@ class TileHandleA1 extends TileHandle {
     SceneManager._scene._spriteset._tilemap.refresh();
   }
 
-  // 根据index查询tile_id是否为范围内(48个组成之一)
-  isCurrentTileByIndex(index,tile_id,layer){
-    if (index < 0) return false;
-    const target_tile_id = $dataMap.data[index + layer * $gameMap.gridCount()];
-    return target_tile_id >= tile_id && target_tile_id < tile_id + 48;
-  }
-
-  // 根据pos查询tile_id是否为范围内(48个组成之一)
-  isCurrentTile(x,y,tile_id,layer){
-    const target_index = x + y * $gameMap.width();
-    return this.isCurrentTileByIndex(target_index,tile_id,layer);
-  };
-
-  // 通过 index 获取 tile_id 的 index
-  getTileIndexByTileId(tile_id){
-    return parseInt((tile_id - this.start_tile_id) / 48);
-  };
-
-  // 通过 pos 获取 tile_index
-  getTileIndexByTilePos(x,y,layer){
-    const tile_id = this.getTileIdByPos(x,y,layer);
-    const tile_index = this.getTileIndexByTileId(tile_id);
-    return tile_index;
-  };
 
   // 通过 pos 检查当前位置是否为 auto_tile
   checkIsAutoTileByPos(x,y,layer){
@@ -87,15 +63,6 @@ class TileHandleA1 extends TileHandle {
     const tile_index = this.getTileIndexByTileId(tile_id);
     const is_special_tile = TileHandleA1.SPECIAL_A1_TILE_ID.includes(tile_index);
     return is_special_tile;
-  };
-
-
-  clearTileLayerByPos(x,y,layer){
-    const target_index = x + y * $gameMap.width();
-    const clear_target_id = $dataMap.data[target_index + layer * $gameMap.gridCount()];
-    if (clear_target_id !== 0){
-      $dataMap.data[target_index + layer * $gameMap.gridCount()] = 0;
-    }
   };
 
   refreshCore(){
@@ -194,7 +161,7 @@ class TileHandleA1 extends TileHandle {
     if (is_auto_tile){
 
       // 根据周围8各自计算出 需要增加的 inner_tile_id
-      let inner_tile_id = this.getCenterTileIdByAround(x,y,tile_id,layer);
+      let inner_tile_id = this.getCenterAutoTileIdByAround(x,y,tile_id,layer);
       tile_id += inner_tile_id
 
     } else {
@@ -210,82 +177,6 @@ class TileHandleA1 extends TileHandle {
 
     $dataMap.data[pos_index + layer * $gameMap.gridCount()] = tile_id;
   }
-
-
-  // 扫描检测周边格子是满足条件, conditions 为一组周边8个格子的情况 -> 0: 不包含 1: 包含  null: 不检测跳过
-  // 超过地图边界这返回 -> 包含
-  conditionScanRoundTiles(conditions, x, y, tile_id, layer){
-
-    const pos_left  = [x-1, y];
-    const pos_up    = [x, y-1];
-    const pos_right = [x+1, y];
-    const pos_down  = [x, y+1];
-    const pos_left_top   = [x-1, y-1];
-    const pos_right_top  = [x+1, y-1];
-    const pos_left_down  = [x-1, y+1];
-    const pos_right_down = [x+1, y+1];
-
-    let result = true;
-    for(let i = 0; i < conditions.length; i++){
-      if (conditions[i] === null){
-        continue;
-      }
-      let dir = i + 1;
-      let exist = conditions[i] === 1;
-
-      if (dir === 1){
-        if (exist !== (this.checkTileIsPadding(pos_left_down[0],pos_left_down[1]) || this.isCurrentTile(pos_left_down[0],pos_left_down[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 2){
-        if (exist !== (this.checkTileIsPadding(pos_down[0],pos_down[1]) || this.isCurrentTile(pos_down[0],pos_down[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 3){
-        if (exist !== (this.checkTileIsPadding(pos_right_down[0],pos_right_down[1]) || this.isCurrentTile(pos_right_down[0],pos_right_down[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 4){
-        if (exist !== (this.checkTileIsPadding(pos_left[0],pos_left[1]) || this.isCurrentTile(pos_left[0],pos_left[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 6){
-        if (exist !== (this.checkTileIsPadding(pos_right[0],pos_right[1]) || this.isCurrentTile(pos_right[0],pos_right[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 7){
-        if (exist !== (this.checkTileIsPadding(pos_left_top[0],pos_left_top[1]) || this.isCurrentTile(pos_left_top[0],pos_left_top[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 8){
-        if (exist !== (this.checkTileIsPadding(pos_up[0],pos_up[1]) || this.isCurrentTile(pos_up[0],pos_up[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      if (dir === 9){
-        if (exist !== (this.checkTileIsPadding(pos_right_top[0],pos_right_top[1]) || this.isCurrentTile(pos_right_top[0],pos_right_top[1],tile_id,layer))){
-          result = false;
-          break;
-        }
-      }
-      
-    }
-    return result;
-  }
-
 
   // 普通A1图块(非Auto)根据左右情况计算出 需要增加的 inner_tile_id
   getTileidByLeftAndRight(x,y,tile_id,layer){
@@ -313,154 +204,6 @@ class TileHandleA1 extends TileHandle {
       inner_target_id = 1;
     }
     return inner_target_id;
-  }
-
-  // 检测周边tile_id状况来获取中心tile的内部tile_id
-  getCenterTileIdByAround(x,y,tile_id,layer){
-
-    let center_inner_target_id;
-    if (this.conditionScanRoundTiles([1,1,1,1,null,1,1,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 0;
-    } 
-    else if (this.conditionScanRoundTiles([1,1,1,1,null,1,0,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 1;
-    }
-    else if (this.conditionScanRoundTiles([1,1,1,1,null,1,1,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 2;
-    }
-    else if (this.conditionScanRoundTiles([1,1,1,1,null,1,0,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 3;
-    }
-    else if (this.conditionScanRoundTiles([1,1,0,1,null,1,1,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 4;
-    }
-    else if (this.conditionScanRoundTiles([1,1,0,1,null,1,0,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 5;
-    }
-    else if (this.conditionScanRoundTiles([1,1,0,1,null,1,1,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 6;
-    }
-    else if (this.conditionScanRoundTiles([1,1,0,1,null,1,0,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 7;
-    }
-    else if (this.conditionScanRoundTiles([0,1,1,1,null,1,1,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 8;
-    }
-    else if (this.conditionScanRoundTiles([0,1,1,1,null,1,0,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 9;
-    }
-    else if (this.conditionScanRoundTiles([0,1,1,1,null,1,1,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 10;
-    }
-    else if (this.conditionScanRoundTiles([0,1,1,1,null,1,0,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 11;
-    }
-    else if (this.conditionScanRoundTiles([0,1,0,1,null,1,1,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 12;
-    }
-    else if (this.conditionScanRoundTiles([0,1,0,1,null,1,0,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 13;
-    }
-    else if (this.conditionScanRoundTiles([0,1,0,1,null,1,1,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 14;
-    }
-    else if (this.conditionScanRoundTiles([0,1,0,1,null,1,0,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 15;
-    }
-    else if (this.conditionScanRoundTiles([null,1,1,0,null,1,null,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 16;
-    }
-    else if (this.conditionScanRoundTiles([null,1,1,0,null,1,null,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 17;
-    }
-    else if (this.conditionScanRoundTiles([null,1,0,0,null,1,null,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 18;
-    }
-    else if (this.conditionScanRoundTiles([null,1,0,0,null,1,null,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 19;
-    }
-    else if (this.conditionScanRoundTiles([1,1,1,1,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 20;
-    }
-    else if (this.conditionScanRoundTiles([1,1,0,1,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 21;
-    }
-    else if (this.conditionScanRoundTiles([0,1,1,1,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 22;
-    }
-    else if (this.conditionScanRoundTiles([0,1,0,1,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 23;
-    }
-    else if (this.conditionScanRoundTiles([1,1,null,1,null,0,1,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 24;
-    }
-    else if (this.conditionScanRoundTiles([0,1,null,1,null,0,1,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 25;
-    }
-    else if (this.conditionScanRoundTiles([1,1,null,1,null,0,0,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 26;
-    }
-    else if (this.conditionScanRoundTiles([0,1,null,1,null,0,0,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 27;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,1,1,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 28;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,1,0,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 29;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,1,1,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 30;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,1,0,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 31;
-    }
-    else if (this.conditionScanRoundTiles([null,1,null,0,null,0,null,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 32;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 33;
-    }
-    else if (this.conditionScanRoundTiles([null,1,1,0,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 34;
-    }
-    else if (this.conditionScanRoundTiles([null,1,null,0,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 35;
-    }
-    else if (this.conditionScanRoundTiles([1,1,null,1,null,0,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 36;
-    }
-    else if (this.conditionScanRoundTiles([0,1,null,1,null,0,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 37;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,0,1,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 38;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,0,0,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 39;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,0,null,1,null,1,1],x,y,tile_id,layer)){
-      center_inner_target_id = 40;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,0,null,1,null,1,0],x,y,tile_id,layer)){
-      center_inner_target_id = 41;
-    }
-    else if (this.conditionScanRoundTiles([null,1,null,0,null,0,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 42;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,0,null,1,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 43;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,0,null,0,null,1,null],x,y,tile_id,layer)){
-      center_inner_target_id = 44;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,1,null,0,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 45;
-    }
-    else if (this.conditionScanRoundTiles([null,0,null,0,null,0,null,0,null],x,y,tile_id,layer)){
-      center_inner_target_id = 46;
-    }
-    return center_inner_target_id;
   }
 }
 
